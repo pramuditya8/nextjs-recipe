3001553b10e85d8813489ae5a3bdc893e2023345
pramuditya8 <sahrulfauzip@gmail.com>
2025-05-19 02:42:00 +0700

    feat: for deployment

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,18 @@ const nextConfig: NextConfig = {
   env: {
     BASE_URL: process.env.BASE_URL,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
